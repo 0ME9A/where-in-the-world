4710858daaf97bcd21ef38cbcf9caf0ada24c148
omega <heyome9a@gmail.com>
2022-07-08 00:48:24 +0530

    react build files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from './App';
-
-const reactElement = document.getElementById("root");
-const root = ReactDOM.createRoot(reactElement);
-root.render(<App/>)
